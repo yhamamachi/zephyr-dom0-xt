5e649e00f4815156375053858076fc5549d0a2bf
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2025-03-11 15:06:12 +0900

    spider: domd: Fix domain name
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- src/dom_cfg/rcar_spider_ca55_domd.c
+++ src/dom_cfg/rcar_spider_ca55_domd.c
@@ -282,7 +282,7 @@ static ssize_t get_ipl_image_size(void* image_info, uint64_t* size)
 }
 
 struct xen_domain_cfg domd_cfg = {
-    .name                 = "DomD helloworld",
+    .name                 = "DomD",
     .machine_dt_compat    = (const char*[]) {"renesas,r8a779f0"},
     .nr_machine_dt_compat = 1,
     .mem_kb               = 0x100000, /* 1Gb */
